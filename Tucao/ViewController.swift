a4e658fa40159c3d8e62a02923a25f2d7b2a8727
qingcai518 <qingcai518@hotmail.co.jp>
2016-05-31 16:42:43 +0900

    Initial Commit

---
+++ Tucao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tucao
+//
+//  Created by user on 2016/05/31.
+//  Copyright © 2016年 kaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
